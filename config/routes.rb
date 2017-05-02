f1a6f2064c6be3961e085b4668b96d45ee5c2592
Donathan Raymond <donathan.raymond@gmail.com>
2017-05-02 10:45:41 -0400

    added carrier-wave and an api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'application#home'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
