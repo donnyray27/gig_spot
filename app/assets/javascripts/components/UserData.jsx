e6bdf3e6d549e4efae2c75ede52ed6dc8e24025e
Donathan Raymond <donathan.raymond@gmail.com>
2017-05-03 19:56:54 -0400

    able to create new gigs successfully

--- app/assets/javascripts/components/UserData.jsx
+++ app/assets/javascripts/components/UserData.jsx
@@ -7,12 +7,19 @@ class UserData extends React.Component {
       genres: this.props.genres,
       gigs: this.props.gigs,
       gigRequests: this.props.gigRequests,
-      bandRequests: this.props.bandRequests
+      bandRequests: this.props.bandRequests,
+      addingAGig: false
     }
     this.handleGigUpdate = this.handleGigUpdate.bind(this)
+    this.handleNewGig = this.handleNewGig.bind(this)
+    this.handleCreate = this.handleCreate.bind(this)
   }
 
-
+  handleCreate(){
+    this.setState({
+      addingAGig: !this.state.addingAGig
+    })
+  }
   handleGigUpdate(gig) {
       let userId = this.props.user.id
       fetch(`/api/v1/users/${userId}/gigs/${gig.id}`, {
@@ -33,28 +40,58 @@ class UserData extends React.Component {
       .then(response => response.json())
       .then(response => {
         console.log(response)
-        this.setState({ gigs: [response]});
+        this.setState({ gigs: response});
+      })
+  }
+
+  handleNewGig(gig) {
+      let userId = this.props.user.id
+      fetch(`/api/v1/users/${userId}/gigs`, {
+      credentials: 'same-origin',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(gig)
+    })
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        this.setState({ gigs: response});
       })
   }
 
   render() {
+
+    let addGig = this.state.addingAGig ? <div><GigNew onClick={this.handleNewGig}/>
+                                        <button onClick={this.handleCreate}>Cancel</button></div> :
+                                          <button onClick={this.handleCreate}>Add a Gig</button>
     return(
+      /*<ComboDatePicker order="mdy" minDate={new Date('2017-01-01')}
+                                  maxDate={new Date('2018-12-31')}/>*/
       <div>
-        <div className="row">
-            <ComboDatePicker order="mdy" minDate={new Date('2017-01-01')}
-                                        maxDate={new Date('2018-12-31')}/>
-
-        </div>
-
         <h1>{this.state.user.first_name} {this.state.user.last_name}</h1>
         <h2>{this.state.user.bio}</h2>
         <InstrumentsContainer instruments={this.state.instruments}/>
+
         <GenresContainer genres={this.state.genres} />
+
+        {addGig}
+
         <GigsContainer gigs={this.state.gigs}
           onUpdate={this.handleGigUpdate} />
+
         <GigRequestsContainer
           gigRequests={this.state.gigRequests}
           />
+
         <BandRequestsContainer bandRequests={this.state.bandRequests}/>
       </div>
 
--- app/controllers/api/v1/gigs_controller.rb
+++ app/controllers/api/v1/gigs_controller.rb
@@ -1,19 +1,27 @@
 class Api::V1::GigsController < ApplicationController
 
-  skip_before_filter  :verify_authenticity_token
+  skip_before_action  :verify_authenticity_token
 
   def index
     @user = User.all
   end
 
-  # {"id"=>"1", "venue"=>"Crown Royal Hotel", "address"=>"1 Main St. Woburn, MA", "dateTime"=>"2017-05-30T19:00:00.000Z", "description"=>"playaing a gala for my church", "user_id"=>"1", "gig"=>{"id"=>"1", "venue"=>"Crown Royal Hotel", "address"=>"1 Main St. Woburn, MA", "description"=>"playaing a gala for my church"}}
+  def create
+    gig = Gig.new(gig_params)
+    gig.event_date = params[:dateTime]
+    user = User.find(params[:user_id])
+    gig.user = user
+    gig.save
+    user_gigs = Gig.where(user_id: params[:user_id])
+    render json: user_gigs
+  end
 
   def update
     gig = Gig.find(params[:gig][:id])
     gig.update(gig_params)
     gig.update(event_date: params[:dateTime])
     user_gigs = Gig.where(user_id: params[:user_id])
-    render json: gig
+    render json: user_gigs
   end
 
   def gig_params
