1580994f1f67829c9a8fc3a2335510df99781b05
Donathan Raymond <donathan.raymond@gmail.com>
2017-05-02 23:09:19 -0400

    trying to get a date-time picker

--- app/assets/javascripts/components/UserData.jsx
+++ app/assets/javascripts/components/UserData.jsx
@@ -8,6 +8,12 @@ class UserData extends React.Component {
   render() {
     return(
       <div>
+        <div className="row">
+            <ComboDatePicker order="mdy" minDate={new Date('2017-01-01')}
+                                        maxDate={new Date('2018-12-31')}/>
+
+        </div>
+
         <h1>{this.props.data.first_name} {this.props.data.last_name}</h1>
         <h2>{this.props.data.bio}</h2>
       </div>
