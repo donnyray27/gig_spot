8c66824c22651f5b656c98eaa5944f9908436ba9
Donathan Raymond <donathan.raymond@gmail.com>
2017-05-12 07:53:09 -0400

    fixed missing controller action

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :gig_requests, only: [:index, :show, :update, :destroy]  do
+      resources :gig_requests, only: [:index, :show, :create, :update, :destroy]  do
         resources :auditions, only: [:create, :destroy]
       end
     end
